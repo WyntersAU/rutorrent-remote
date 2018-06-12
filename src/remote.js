fc55f91a59864e657232d7b848424a45f7ae68a8
Edward Akers <edward@akers.me>
2018-06-13 00:42:36 +1000

    Added:   - Tooltips for the torrent names   - Divider within context menu   - Remove submenu Updated:   - Context Menu logic. Now spawns with showMenu rather than using a ContextMenuTrigger, allowing for more versatility with it's usages.   - Context Menu click handling logic.

--- src/remote.js
+++ src/remote.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import ReactTable from 'react-table'
 import axios from 'axios'
-import { ContextMenu, MenuItem, ContextMenuTrigger } from "react-contextmenu";
+import { ContextMenu, MenuItem, SubMenu, ContextMenuTrigger } from "react-contextmenu";
+import { showMenu } from 'react-contextmenu/modules/actions'
 
 var utilities = require("utilities.js");
 var columns = [
-    { Header: "Name", accessor: "name", width: 275 },
-    { Header: "Status", accessor: "status", width: 57 },
+    { Header: "Name", accessor: "name", width: 250, Cell: row => (
+            <div title={row.value}>
+                {row.value}
+            </div>
+        )},
+    { Header: "Status", accessor: "status", width: 80 },
     { Header: "Size", accessor: "size", width: 68 },
     { Header: "Done", accessor: "done", width: 49 },
     { Header: "Downloaded", accessor: "downloaded", width: 79 },
@@ -16,140 +21,132 @@ var columns = [
     { Header: "UL", accessor: "ul", width: 75 },
     { Header: "DL", accessor: "dl", width: 85 },
     { Header: "Added", accessor: "added", width: 69, show: false }
-  ];
+];
 
 class Popup extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [],
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+        };
 
-    this.getTorrents = this.getTorrents.bind(this);
-    this.handleOnResizeChange = this.handleOnResizeChange.bind(this);
-
-    this.getTorrents();
-  }
-
-
-  async getTorrents() {
-  	var params = new URLSearchParams() 
-  	params.append('mode', 'list'); 
-    params.append('cmd', 'd.custom=addtime');
-    var url = (await browser.storage.local.get('url')).url.replace(/(https?:\/\/)/, '$1' + (await browser.storage.local.get('username')).username + ':' + (await browser.storage.local.get('password')).password + '@');
-
-  	var torrents = (await axios.post(url + '/plugins/httprpc/action.php', params)).data;
-	  var data = [];
-
-  	for (var hash in torrents.t) {
-  		var info = torrents.t[hash];
-
-  		var name = info[4];
-  		var size = info[5];
-  		var downloaded = info[8];
-  		var uploaded = info[9];
-  		var ratio = info[10];
-  		var ul = info[11];
-  		var dl = info[12];
-      var added = info[34];
-
-  		var torrent = {
-  			name: name,
-  			status: "Seeding", 
-  			size: utilities.ToBytes(size),
-  			//size_b: size,
-  			done: +(downloaded / size * 100).toFixed(2) + "%",
-  			downloaded: utilities.ToBytes(+downloaded),
-  			//downloaded_b: downloaded,
-  			uploaded: utilities.ToBytes(+uploaded),
-  			//uploaded_b: uploaded,
-  			ratio: +(ratio / 1000).toFixed(3),
-  			ul: utilities.ToSpeed(+ul),
-  			dl: utilities.ToSpeed(+dl),
-        added: added
-  		};
-			if (info[1] != 0) {	
-				torrent.status = 'Started';
-				if ((info[4] == 0) || (info[29] == 0)) {
-					torrent.status = 'Paused';
-				}
-			}
-			if (info[24] != 0) {
-				torrent.status = 'Hashing';
-			}
-			if (info[2] != 0) {
-				torrent.status = 'Error';
-			}
-			if (+(torrent.downloaded / torrent.size * 100) == 100) {
-				torrent.status = 'Seeding';
-}
-
-  		data.push(torrent);
+        this.getTorrents = this.getTorrents.bind(this);
+        this.handleOnResizeChange = this.handleOnResizeChange.bind(this);
+        this.getTorrents();
     }
-  	this.setState({
-  		data: data
-  	});
 
-    setTimeout(this.getTorrents, 1000);
-  }
+    async getTorrents() {
+        var params = new URLSearchParams()
+        params.append('mode', 'list');
+        params.append('cmd', 'd.custom=addtime');
+        var url = (await browser.storage.local.get('url')).url.replace(/(https?:\/\/)/, '$1' + (await browser.storage.local.get('username')).username + ':' + (await browser.storage.local.get('password')).password + '@');
+        var torrents = (await axios.post(url + '/plugins/httprpc/action.php', params)).data;
+        var data = [];
 
-  handleResized() {
-  	var state = JSON.parse(localStorage.getItem('table'));
-  	
-  	if (!state) {
-  		state = [];
-  		columns.forEach((column) => {
-  			state.push({
-  				id: column.accessor,
-  				value: column.width
-  			});
-  		});
-  	}
-  	return state;
-  }
-  handleOnResizeChange(state) {
-  	localStorage.setItem('table', JSON.stringify(state));
-  	this.forceUpdate();
-  }
+        for (var hash in torrents.t) {
+            var info = torrents.t[hash];
+            var torrent = {
+                name: info[4],
+                status: "Unknown",
+                size: utilities.ToBytes(info[5]),
+                done: +(info[8] / info[5] * 100).toFixed(2),
+                downloaded: utilities.ToBytes(+info[8]),
+                uploaded: utilities.ToBytes(+info[9]),
+                ratio: +(info[10] / 1000).toFixed(3),
+                ul: utilities.ToSpeed(+info[11]),
+                dl: utilities.ToSpeed(+info[12]),
+                added: info[34]
+            };
+            if (torrent.done > 0) {
+                torrent.status = 'Downloading';
+            }
+            if (torrent.done == 100) {
+                torrent.status = 'Seeding';
+            }
+            if (torrent.done == 0) {
+                torrent.status = 'Started';
+            }
+            torrent.done = torrent.done + '%';
+            data.push(torrent);
+        }
+        this.setState({ data: data });
+        setTimeout(this.getTorrents, 1000);
+    }
 
-  render() {
-    return (
-      <div>
-        <ContextMenuTrigger id="start-stop-delete" holdToDisplay="1000">
-	        <ReactTable
-	        	  noDataText="Fetching information from ruTorrent"
+    handleResized() {
+        var state = JSON.parse(localStorage.getItem('table'));
 
-	              data={this.state.data}
-	              columns={columns}
-	              pageSize={this.state.data.length || 3}
-	              showPagination={false}
-                defaultSorted={[
-                  {
-                    id: "added",
-                    desc: true
-                  }
-                ]}
-	              onResizedChange={this.handleOnResizeChange}
-	              resized={this.handleResized()}
-	              style={{
-	                  height: 'auto',
-	              }}
-	              className="-highlight"
-	        />
-	    </ContextMenuTrigger>
+        if (!state) {
+            state = [];
+            columns.forEach((column) => {
+                state.push({ id: column.accessor, value: column.width });
+            });
+        }
+        return state;
+    }
+    handleOnResizeChange(state) {
+        localStorage.setItem('table', JSON.stringify(state));
+        this.forceUpdate();
+    }
 
-        <ContextMenu id='start-stop-delete'>
-          <MenuItem onClick={this.handleCellClick}> 
-            Start
-          </MenuItem>
-          <MenuItem>
-          	Stop
-          </MenuItem>
-        </ContextMenu>
-      </div>
-    );
-  }
-} 
+    handleContextMenuClick_Start(e, state) {
+        var row = state.row.row;
+    }
+    handleContextMenuClick_Stop(e, state) {
+        var row = state.row.row;
+    }
+    handleContextMenuClick_Remove(e, state) {
+        var row = state.row.row;
+    }
+    handleContextMenuClick_RemoveFiles(e, state) {
+        var row = state.row.row;
+    }
+
+    render() {
+        return (
+            <div>
+                <ReactTable
+                    noDataText="Fetching information from ruTorrent"
+                    data={this.state.data}
+                    columns={columns}
+                    pageSize={this.state.data.length || 3}
+                    showPagination={false}
+                    defaultSorted={[{
+                        id: "added",
+                        desc: true
+                    }]}
+                    onResizedChange={this.handleOnResizeChange}
+                    resized={this.handleResized()}
+                    style={{
+                        height: 'auto',
+                    }}
+                    className="-highlight"
+                    getTdProps={(state, rowInfo, column, instance) => {
+                        return {
+                            onContextMenu: e => {
+                                e.preventDefault();
+                                showMenu({
+                                    position: {x: e.nativeEvent.clientX, y: e.nativeEvent.clientY},
+                                    id: 'start-stop-delete'
+                                });
+                                this.setState({
+                                    row: rowInfo
+                                });
+                            }
+                        }
+                    }}/>
+                <ContextMenu id='start-stop-delete'>
+                    <MenuItem onClick={this.handleContextMenuClick_Start} data={this.state}>Start</MenuItem>
+                    <MenuItem onClick={this.handleContextMenuClick_Stop} data={this.state}>Stop</MenuItem>
+                    <MenuItem divider />
+                    <SubMenu title={'Remove'} onClick={this.handleContextMenuClick_Remove} data={this.state}>
+                        <MenuItem onClick={this.handleContextMenuClick_RemoveFiles} data={this.state}>Remove and Delete Files</MenuItem>
+                    </SubMenu>
+                </ContextMenu>
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(<Popup />, document.getElementById('app'));
 export default Popup
